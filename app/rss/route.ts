14c3d3a642a3b9e9b7a3028b44dbd57f26f5b36f
Taylor Sabbag <taylorsabbag@gmail.com>
2024-11-11 16:32:18 -0500

    feat: Change title and description of RSS feed.

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -1,42 +1,42 @@
-import { baseUrl } from 'app/sitemap'
-import { getBlogPosts } from 'app/blog/utils'
+import { baseUrl } from "app/sitemap";
+import { getBlogPosts } from "app/blog/utils";
 
 export async function GET() {
-  let allBlogs = await getBlogPosts()
+	const allBlogs = await getBlogPosts();
 
-  const itemsXml = allBlogs
-    .sort((a, b) => {
-      if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
-        return -1
-      }
-      return 1
-    })
-    .map(
-      (post) =>
-        `<item>
+	const itemsXml = allBlogs
+		.sort((a, b) => {
+			if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
+				return -1;
+			}
+			return 1;
+		})
+		.map(
+			(post) =>
+				`<item>
           <title>${post.metadata.title}</title>
           <link>${baseUrl}/blog/${post.slug}</link>
-          <description>${post.metadata.summary || ''}</description>
+          <description>${post.metadata.summary || ""}</description>
           <pubDate>${new Date(
-            post.metadata.publishedAt
-          ).toUTCString()}</pubDate>
-        </item>`
-    )
-    .join('\n')
+						post.metadata.publishedAt,
+					).toUTCString()}</pubDate>
+        </item>`,
+		)
+		.join("\n");
 
-  const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
+	const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
   <rss version="2.0">
     <channel>
-        <title>My Portfolio</title>
+        <title>Taylor Sabbag's Musings</title>
         <link>${baseUrl}</link>
-        <description>This is my portfolio RSS feed</description>
+        <description>My blog's RSS feed.</description>
         ${itemsXml}
     </channel>
-  </rss>`
+  </rss>`;
 
-  return new Response(rssFeed, {
-    headers: {
-      'Content-Type': 'text/xml',
-    },
-  })
+	return new Response(rssFeed, {
+		headers: {
+			"Content-Type": "text/xml",
+		},
+	});
 }
