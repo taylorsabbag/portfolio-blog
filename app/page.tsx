191ab45eeee7b1cbbd5f06bed52a5ef879985e5f
Taylor Sabbag <taylorsabbag@gmail.com>
2024-11-15 14:50:58 -0500

    Add ExperienceList to main page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { CustomMDX } from "./components/mdx";
+import ExperienceList from "./components/ExperienceList";
 
 export default function Page() {
 	return (
@@ -6,19 +7,19 @@ export default function Page() {
 			<h1 className="mb-8 text-2xl font-semibold tracking-tighter">
 				Things About Me (Taylor Sabbag)
 			</h1>
-			<article className="prose">
+			<article className="prose mb-8">
 				<CustomMDX
-					source={`Hey there! I'm a software engineer who can't help but get excited about solving puzzles through code. From crafting mobile experiences with React Native to building backend systems with Express.js, I'm driven by the thrill of creating technology that actually makes a difference in people's lives.
+					source={`I'm currently the full-stack lead software engineer at Evexia Inc. building a real revolution in mental health.
 
-My engineering journey started with a simple curiosity that turned into a full-blown passion. I believe that the best software isn't just about clean code or fancy features – it's about understanding real problems and building solutions that click with the people using them.
+I code mostly for the web using technologies like JavaScript, TypeScript, Python, React, React Native, HTML, CSS, Node, Express.js, Next.js, Svelte, SvelteKit, SQL, PostgresSQL, and MySQL. Yeah, that's a lot, but such is the web these days. However, I love going deep particularly on understanding the eccentricities and capabilities of JavaScript and the web APIs. I use Git and GitHub to manage my software versioning.
 
-You can check out my experiments and coding adventures (including some questionably necessary isEven methods) on [GitHub](https://github.com/taylorsabbag). I'm also working on sharing my journey and thoughts at [taylorsabbag.dev](https://taylorsabbag.dev).
-
-As a full-stack developer, I love diving into both sides of the tech world – from making things look and feel great on the front end to ensuring everything runs smoothly behind the scenes. The JavaScript/TypeScript ecosystem is my playground, but I'm always eager to explore new technologies that could help build better solutions.
-
-I get a kick out of tackling tricky problems and learning new things along the way. Whether it's optimizing a sluggish API or debugging a mysterious React component, I'm up for the challenge. I'm looking to connect with others who share this enthusiasm for using technology to build stuff that matters.`}
+I currently live in Hamilton, Ontario, Canada (near Toronto if you're from outside Canada), but I'm open to opportunities around the world!`}
 				/>
 			</article>
+			<h2 className="mb-8 text-2xl font-semibold tracking-tighter">
+				Places I've Worked
+			</h2>
+			<ExperienceList />
 		</section>
 	);
 }
